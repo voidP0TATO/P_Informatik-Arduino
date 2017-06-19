deebd9e09fa8638427342e3cd7cecd54b42328c8
Stephan <stephan_hopperdietzel@web.de>
2017-06-19 14:56:51 +0200

    Update SensordatenLesen0.1.ino

--- SensordatenLesen0.1.ino
+++ SensordatenLesen0.1.ino
@@ -8,10 +8,13 @@ int measurecount = 1;//how many measure are taken for the average
 int dry; //used to toggle on the pump
 int wet; //used to toggle off the pump
 boolean pump;//determens wether the pump should be on or off
+int capacitance, temperature; 
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
+  dry = 350;
+  wet = 500;
 
   sensor.begin(); // reset sensor
   delay(1000); // boot up
@@ -27,21 +30,19 @@ void setup() {
 
 void loop() {
   while (sensor.isBusy()) delay(50); // available since FW 2.3
+
+  capacitance = getCapacitance(delaytime,measurecount);
+  temperature = getTemperature(delaytime,measurecount);
   Serial.print("Feuchtigkeit: ");
-  Serial.print(getCapacitance(delaytime,measurecount)); //read capacitance register
+  Serial.print(dry);
+  Serial.print(capacitance); //read capacitance register
   Serial.print(", Temperatur: ");
-  Serial.println(getTemperature(delaytime,measurecount)); //temperature register
+  Serial.println(temperature); //temperature register
   //Serial.print(", Belichtung: ");
   //Serial.println(sensor.getLight(true)); //request light measurement, wait and read light register
   sensor.sleep();
 
   //logic:
-  if(toodry(delaytime,measurecount)){
-    pump = true;
-  }
-  if(toowet(delaytime,measurecount)){
-    pump = false;
-  }
   refreshpump();
   delay(50);
 }
@@ -74,26 +75,35 @@ int getTemperature(int dly, int mc){
   return r/mc;
 }
 
-boolean toodry(int dly,int mc){
-  if(getCapacitance(dly,mc) < dry){
+boolean toodry(){
+  if(capacitance < dry){
     return true;
   }
   return false;
 }
 
-boolean toowet(int dly, int mc){
-  if(getCapacitance(dly, mc) > wet){
+boolean toowet(){
+  if(capacitance > wet){
       return true;
   }
   return false;
 }
 
 void refreshpump(){
+  if(toodry()){
+    pump = true;
+  }
+  if(toowet()){
+    pump = false;
+  }
+  Serial.print(pump);
   if(pump){
     analogWrite(pwm_a, 255);
+    Serial.println("Pumpe sollte laufen");
   }
   else{
     analogWrite(pwm_a, 0);
+    Serial.println("Pumpe sollte nicht laufen");
   }
 }
 
